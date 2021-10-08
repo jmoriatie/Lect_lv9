135a7ce37356caa64c5ae37c90a06a9069515812
jmoriatie <jmoriatie@192.168.145.195>
2021-10-08 09:08:09 +0900

    RPG -작성중

--- RPG/src/main/FileController.java
+++ RPG/src/main/FileController.java
@@ -1,5 +1,33 @@
 package main;
 
-public class FileController {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 
+public class FileController {
+	FileWriter fw = null;
+	FileReader fr = null;
+	BufferedReader br = null;
+	
+	String fileName = "list.txt";
+	
+	File file = new File(fileName);
+	
+	public static FileController instance = new FileController(); 
+	
+	private FileController() {
+		
+	}
+	
+	public void save() {
+		
+	}
+	
+	public void load() {
+		
+	}
+
+
+	
 }
--- RPG/src/main/Main.java
+++ RPG/src/main/Main.java
@@ -12,11 +12,12 @@ public class Main {
 	private static PlayerController pc = PlayerController.instance;
 	private static GuildController gc = GuildController.instance;
 	private static ShopController shop = ShopController.instance;
+	private static FileController fc = FileController.instance;
 	
 	public static void mainGame() {
 		while(true) {
 			System.out.println("========== RPG GAME ==========");
-			System.out.print("[1. 유닛관리][2. 상점][3. 인벤토리][4. 종료] : ");
+			System.out.print("[1. 유닛관리][2. 상점][3. 인벤토리][4. 저장][5. 불러오기][6. 종료] : ");
 			String input = sc.next();
 			int select = -1;
 			try {
@@ -34,7 +35,13 @@ public class Main {
 			else if(select == 3) { // 인벤토리
 				pc.InventoryMenu();
 			}
-			else if(select == 4) { // 종료
+			else if(select == 4) { // 저장
+
+			}
+			else if(select == 5) { // 불러오기
+
+			}
+			else if(select == 6) { // 종료
 				System.out.println("[게임 종료]");
 				break;
 			}
